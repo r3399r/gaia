1393eb6308b1fa4ac50eb45d6af55f85a93af782
yue.chang <lamplighter.planet@gmail.com>
2025-06-23 01:50:20 +0800

    aws rds

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ganymede
+# gaia
+
+infrastructure repo
---
+++ infra.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+env=$1
+project=gaia
+
+echo ====================================================================================
+echo env: $env
+echo project: $project
+echo ====================================================================================
+
+echo deploy AWS infrastructure...
+aws cloudformation package --template-file aws/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway-ap-east-2
+aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project --no-fail-on-empty-changeset --s3-bucket y-cf-midway-ap-east-2 --capabilities CAPABILITY_NAMED_IAM
+echo ====================================================================================
